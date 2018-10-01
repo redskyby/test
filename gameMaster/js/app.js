be2d9c174b1fd32fccc19a604dfc0da0e708dc5d
redskyby <pashadocenko@gmail.com>
2018-10-01 20:48:08 +0300

    for delet2

--- gameMaster/js/app.js
+++ gameMaster/js/app.js
@@ -20,3 +20,30 @@ function formNOTfocus(){
    document.getElementById('header_button').classList.remove('animation_button');
 };
 
+
+//TABS/
+var tabsLink = document.querySelectorAll('.nav-tabs a');
+var tabsElement = document.querySelectorAll('.tab-pane');
+var i;
+
+tabsElement[0].classList.add('tab-open');
+
+var showTabs = function(e) {
+  e.preventDefault();
+  var targetElement = document.querySelectorAll(this.getAttribute('href'));
+  for (i = 0; i < targetElement.length; i++) {
+    if (targetElement[i].classList.contains('tab-open')) {
+      return false;
+    } else targetElement[i].classList.add('tab-open');
+  }
+
+  targetElement = document.querySelectorAll(".tab-pane.tab-open:not("+ this.getAttribute('href')+")");
+  for (i = 0; i < targetElement.length; i++) {
+      targetElement[i].classList.remove('tab-open');
+  }
+
+};
+
+for (i = 0; i < tabsLink.length; i++) {
+  tabsLink[i].addEventListener('click', showTabs);
+}
