7600ff5ce9365d147f462c28407534653ba53a60
redskyby <pashadocenko@gmail.com>
2018-10-03 22:05:58 +0300

    fd4

--- gameMaster/config.rb
+++ gameMaster/config.rb
@@ -2,10 +2,10 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "../"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
