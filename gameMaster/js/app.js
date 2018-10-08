6899e51386c7bd2f231f109ac832445f72e1ec5c
redskyby <pashadocenko@gmail.com>
2018-10-09 01:50:39 +0300

    fd5

--- gameMaster/js/app.js
+++ gameMaster/js/app.js
@@ -47,3 +47,23 @@ var showTabs = function(e) {
 for (i = 0; i < tabsLink.length; i++) {
   tabsLink[i].addEventListener('click', showTabs);
 }
+
+//Slider  
+var slides = document.querySelectorAll('.sliderFabe');
+
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,3000);
+
+function nextSlide(){
+	goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+	goToSlide(currentSlide-1);
+}
+
+function goToSlide(n){
+	slides[currentSlide].className = 'sliderFabe';
+	currentSlide = (slides.length+n)%slides.length;
+	slides[currentSlide].className = 'sliderFabe Showing';
+}
