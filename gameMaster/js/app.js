577b21ea268212a6e82451ce076ec7c8414c0cc7
redskyby <pashadocenko@gmail.com>
2018-11-21 01:40:02 +0300

    corrected js

--- gameMaster/js/app.js
+++ gameMaster/js/app.js
@@ -21,15 +21,14 @@ function formNOTfocus(){
 };
 
 //search RegExp
-var input = document.getElementById('input');
-var bab_number = document.querySelector(".header_form_search_error");
-var pattern = /^\s{0,1}([a-z0-9-а-яё]{1,}\s*){0,3}$/i;
-var value;
 
 input.addEventListener("input", search_error );
 
 function search_error(value) {
-   var  newValye = this.value;
+    var bab_number = document.querySelector(".header_form_search_error");
+    var pattern = /^\s{0,1}([a-z0-9-а-яё]{1,}\s*){0,3}$/i;
+    var value;
+    var  newValye = this.value;
   if(pattern.test(newValye)){
         bab_number.style.display= "none"
   }else{
@@ -41,46 +40,33 @@ function search_error(value) {
 
 //TABS/
 var tabsLink = document.querySelectorAll('.tab');
-
-var tabLI = document.querySelectorAll('.nav-tabs li');
 var tabsElement = document.querySelectorAll('.tab-pane');
-var i;
 
 tabsElement[0].classList.add('tab-open');
 
 var showTabs = function(e) {
+  var i;
   e.preventDefault();
   var targetElement = document.querySelectorAll(this.getAttribute('href'));
   for (i = 0; i < targetElement.length; i++) {
     if (targetElement[i].classList.contains('tab-open')) {
       return false;
-    } else {targetElement[i].classList.add('tab-open');
-           }
+    } else {targetElement[i].classList.add('tab-open');}
   }
    
 
 
-  targetElement = document.querySelectorAll(".tab-pane.tab-open:not("+ this.getAttribute('href')+")");
+  targetElement = document.querySelectorAll(".tab-pane.tab-open:not("+this.getAttribute('href')+")");
   for (i = 0; i < targetElement.length; i++) {
       targetElement[i].classList.remove('tab-open');
   }
-
 };
 
 for (i = 0; i < tabsLink.length; i++) {
   tabsLink[i].addEventListener('click', showTabs);
 }
- function oil(e){
-    console.log('1')
-}
-for (i = 0; i < tabsLink.length; i++) {
-  tabLI[i].addEventListener('click' , oil);
-}
-
-
 //Slider  
 var slides = document.querySelectorAll('.sliderFabe');
-
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,3000);
 
